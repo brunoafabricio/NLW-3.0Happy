df3a17ae1c2112a4768e117133d3517329858d86
Bruno <brunof.tech@gmail.com>
2020-10-14 03:38:41 -0300

    Second Day Finished

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import { join } from "path";
+
+import "express-async-errors";
+
+import "./database/connection";
+
+import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
-app.listen(3333);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use("/uploads", express.static(join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
-// REQ / RES
-// localhost:3333
+app.listen(3333);
