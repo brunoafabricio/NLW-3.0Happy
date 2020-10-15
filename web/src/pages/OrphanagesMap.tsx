e00e5405ec72dc099621106b32fac968f3630695
Bruno <brunof.tech@gmail.com>
2020-10-15 03:50:23 -0300

    Third day finished

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,70 +1,86 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { Map, Marker, TileLayer, Popup } from 'react-leaflet'
-import Leaflet from 'leaflet'
+import React, { useEffect, useState } from "react";
 
-import 'leaflet/dist/leaflet.css'
-import '../styles/pages/orphanages-map.css'
-import mapMarkerImg from '../images/map-marker.svg'
+import { Link } from "react-router-dom";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-const mapIcon = Leaflet.icon({
-	iconUrl: mapMarkerImg,
+import "../styles/pages/orphanages-map.css";
 
-	iconSize: [58, 68],
-	iconAnchor: [29, 68],
-})
+import mapMakerImg from "../images/map-marker.svg";
+import mapIcon from "../utils/mapIcon";
+import api from "../services/api";
 
-function OrphanagesMap() {
-	return (
-		<div id="page-map">
-			<aside>
-				<header>
-					<img src={mapMarkerImg} alt="Happy Icon" />
-
-					<h2>Escolha um orfanato no mapa</h2>
-					<p>Muitas crianças estão esperando a sua visita</p>
-				</header>
-
-				<footer>
-					<strong>Porto Alegre</strong>
-					<span>Rio Grande do Sul</span>
-				</footer>
-			</aside>
-
-			<Map center={[-30.0344026, -51.2111683]} zoom={15} style={{ width: '100%', height: '100%' }}>
-				{/* <TileLayer url={"https://a.tile.openstreatmap.org/{z}/{x}/{y}.png"} /> */}
-				<TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-
-
-				<Marker
-					icon={mapIcon}
-					position={[-30.0344026, -51.2111683]}
-				>
-					<Popup
-						closeButton={false}
-						minWidth={240}
-						maxWidth={240}
-						className="map-popup"
-					>
-					</Popup>
-
-				</Marker>
-
-			</Map>
-
-			<Link to="" className="create-orphanage">
-				<FiPlus size={32} color="#fff" />
-			</Link>
-		</div>
-
-
-	);
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
 }
 
+const OrphanagesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
 
 
+  useEffect(() => {
+    api.get("/orphanages").then(({ data }) => {
+      setOrphanages(data);
+    });
+  }, []);
 
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMakerImg} alt="Happy" />
 
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Porto Alegre</strong>
+          <span>Rio Grande do Sul</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-30.0344026, -51.2111683]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        {orphanages.map((orphanage) => {
+          return (
+            <Marker
+              key={orphanage.id}
+              icon={mapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+              >
+                {orphanage.name}
+                <Link to={`/orphanages/${orphanage.id}`}>
+                  <FiArrowRight size="20" color="#FFF" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size="32" color="#fff" />
+      </Link>
+    </div>
+  );
+};
 
 export default OrphanagesMap;
