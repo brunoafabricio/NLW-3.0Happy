50277b9259886b4047cf72694f37ae8bdc0ee42c
Bruno <brunof.tech@gmail.com>
2020-10-16 14:45:58 -0300

    api fix

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,76 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View , Dimensions } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native';
 
 import mapMarker from "../images/map-marker.png";
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../services/api';
+
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
 
 const OrphanagesMap: React.FC = () => {
+
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
   const navigation = useNavigation();
 
+
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, [])
+
   const handleNavigationToOrphanagesDetails = () => {
-    navigation.navigate('OrphanageDetails'); 
+    navigation.navigate('OrphanageDetails');
   }
 
   const handleNavigationToCreateOrphanage = () => {
-    navigation.navigate('SelectMapPosition'); 
+    navigation.navigate('SelectMapPosition');
   }
-  
+
 
   return (
     <View style={styles.container}>
-      <MapView 
+      <MapView
         provider={PROVIDER_GOOGLE}
-        style={styles.map} 
-        initialRegion={{ 
-          latitude: -30.5241472, 
-          longitude: -51.2913536, 
-          latitudeDelta: 0.008, 
-          longitudeDelta:  0.008
-        }} 
+        style={styles.map}
+        initialRegion={{
+          latitude: -30.5241472,
+          longitude: -51.2913536,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
       >
-        <Marker 
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.9
-          }}
-          coordinate={{
-            latitude: -30.5241472, 
-            longitude: -51.2913536, 
-          }}
-        >
-          <Callout tooltip onPress={handleNavigationToOrphanagesDetails}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das Meninas</Text>
-            </View>
-          </Callout>
-        </Marker>
+        {orphanages.map(orphanage => {
 
-        <Marker 
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.9
-          }}
-          coordinate={{
-            latitude: -30.5241472, 
-            longitude: -51.2913536, 
-          }}
-        >
-          <Callout tooltip onPress={handleNavigationToOrphanagesDetails}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Orfanato Pedra de Guaratiba</Text>
-            </View>
-          </Callout>
-        </Marker>
+          return (
+            <Marker
+              key={orphanage.id}
+              icon={mapMarker}
+              calloutAnchor={{
+                x: 2.7,
+                y: 0.9
+              }}
+              coordinate={{
+                latitude: orphanage.latitude,
+                longitude: orphanage.longitude,
+              }}
+            >
+              <Callout tooltip onPress={handleNavigationToOrphanagesDetails}>
+                <View style={styles.calloutContainer}>
+                  <Text style={styles.calloutText}>{orphanage.name}</Text>
+                </View>
+              </Callout>
+            </Marker>
+          );
+        })}
       </MapView>
 
       <View style={styles.footer}>
         <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
-        
+
         <RectButton style={styles.createOrphanageButton} onPress={handleNavigationToCreateOrphanage}>
           <Feather name="plus" size={20} color="#fff" />
         </RectButton>
@@ -83,7 +88,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  
+
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height
